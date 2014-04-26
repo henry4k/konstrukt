154ef1a8ad90f4d041900ad660ebc8cf597c836e
Henry Kielmann <henrykielmann@gmail.com>
2014-04-26 11:20:20 +0200

    Fixes

--- merge-subtree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]; then
-    echo "Merges the given subtree branch into the current one." 1>&2
-    echo "Usage: $0 <subtree-branch>" 1>&2
-    exit 1
-fi
-git merge --squash -s subtree --no-commit "$1"
