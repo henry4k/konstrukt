dcd6220ff4210455e4410838390c363b252b2e02
Henry Kielmann <henrykielmann@gmail.com>
2014-05-04 16:22:11 +0200

    Added Lua bindings for controls module.

--- src/Controls.cpp
+++ src/Controls.cpp
@@ -1,18 +1,22 @@
 #include <string.h>
 #include <vector>
 #include <map>
+
 #include "Common.h"
 #include "Config.h"
 #include "OpenGL.h"
 #include "Window.h"
+#include "Lua.h"
 #include "Squirrel.h"
 #include "Controls.h"
 #include "KeyboardBindings.h"
 #include "MouseBindings.h"
 #include "JoystickBindings.h"
 
+
 static const int MAX_CONTROL_NAME_LENGTH = 31;
 
+bool RegisterControlsInLua();
 bool CreateKeyBindingFromString( const char* str, int keyControl );
 bool CreateAxisBindingFromString( const char* str, int axisControl );
 
@@ -50,7 +54,11 @@ bool InitControls()
     g_KeyControls.clear();
     g_AxisControls.clear();
 
-    return InitKeyboardBindings() && InitMouseBindings() && InitJoystickBindings();
+    return
+        InitKeyboardBindings() &&
+        InitMouseBindings() &&
+        InitJoystickBindings() &&
+        RegisterControlsInLua();
 }
 
 void DestroyControls()
@@ -269,6 +277,65 @@ bool CreateAxisBindingFromString( const char* str, int axisControl )
         return true;
 
     return false;
+}
+
+
+// --- lua bindings ---
+
+const char* KEY_CONTROL_ACTION_EVENT_NAME = "KeyControlAction";
+const char* AXIS_CONTROL_ACTION_EVENT_NAME = "AxisControlAction";
+
+int g_KeyControlActionEvent = LUA_INVALID_EVENT;
+int g_AxisControlActionEvent = LUA_INVALID_EVENT;
+
+void OnLuaKeyControlAction( const char* name, bool pressed, void* context )
+{
+    lua_State* l = GetLuaState();
+    lua_pushstring(l, name);
+    lua_pushboolean(l, pressed);
+    FireLuaEvent(l, g_KeyControlActionEvent, 2, false);
+}
+
+int Lua_RegisterKeyControl( lua_State* l )
+{
+    const char* name = luaL_checkstring(l, 1);
+
+    const bool success = RegisterKeyControl(name, OnLuaKeyControlAction, NULL, NULL);
+    lua_pushboolean(l, success);
+    return 1;
+}
+
+void OnLuaAxisControlAction( const char* name, float absolute, float delta, void* context)
+{
+    lua_State* l = GetLuaState();
+    lua_pushstring(l, name);
+    lua_pushnumber(l, absolute);
+    lua_pushnumber(l, delta);
+    FireLuaEvent(l, g_AxisControlActionEvent, 3, false);
+}
+
+int Lua_RegisterAxisControl( lua_State* l )
+{
+    const char* name = luaL_checkstring(l, 1);
+
+    const bool success = RegisterAxisControl(name, OnLuaAxisControlAction, NULL, NULL);
+    lua_pushboolean(l, success);
+    return 1;
+}
+
+bool RegisterControlsInLua()
+{
+    g_KeyControlActionEvent = RegisterLuaEvent(KEY_CONTROL_ACTION_EVENT_NAME);
+    if(g_KeyControlActionEvent == LUA_INVALID_EVENT)
+        return false;
+
+    g_AxisControlActionEvent = RegisterLuaEvent(AXIS_CONTROL_ACTION_EVENT_NAME);
+    if(g_AxisControlActionEvent == LUA_INVALID_EVENT)
+        return false;
+
+    return
+        RegisterFunctionInLua("RegisterKeyControl", Lua_RegisterKeyControl) &&
+        RegisterFunctionInLua("RegisterAxisControl", Lua_RegisterAxisControl);
 }
 
 
