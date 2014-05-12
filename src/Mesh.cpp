4bbdcc974a30bbefb7648cced77a0b371bc13430
Henry Kielmann <henrykielmann@gmail.com>
2014-05-12 13:29:33 +0200

    Added Lua bindings for mesh module.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,6 +1,7 @@
 #include "Common.h"
 #include "OpenGL.h"
 #include "Squirrel.h"
+#include "Lua.h"
 #include "Mesh.h"
 
 bool CreateMesh( Mesh* mesh, const MeshBuffer* buffer )
@@ -56,6 +57,44 @@ void DrawMesh( const Mesh* mesh )
     {
         glDrawArrays(mesh->primitiveType, 0, mesh->size);
     }
+}
+
+
+// --- lua bindings ---
+
+const char* MESH_TYPE = "Mesh";
+
+int Lua_Mesh_destructor( lua_State* l )
+{
+    const Mesh* mesh = (Mesh*)lua_touserdata(l, 1);
+    FreeMesh(mesh);
+    return 0;
+}
+
+int Lua_CreateMesh( lua_State* l )
+{
+    const MeshBuffer* buffer = GetMeshBufferFromLua(l, 1);
+
+    Mesh* mesh = (Mesh*)PushUserDataToLua(l, MESH_TYPE, sizeof(Mesh));
+    if(CreateMesh(mesh, buffer))
+    {
+        return 1;
+    }
+    else
+    {
+        lua_pop(l, 1);
+        luaL_error(l, "Failed to create mesh!");
+        return 0;
+    }
+}
+
+bool RegisterMeshInLua()
+{
+    if(!RegisterUserDataTypeInLua(MESH_TYPE, Lua_Mesh_destructor))
+        return false;
+
+    return
+        RegisterFunctionInLua("CreateMesh", Lua_CreateMesh);
 }
 
 
