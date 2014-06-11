6925ff13d0ea593eda5883650421286eae9384fe
Henry Kielmann <henrykielmann@gmail.com>
2014-06-11 22:47:24 +0200

    Fixed Matrix4 class.

--- core/Matrix4.lua
+++ core/Matrix4.lua
@@ -4,43 +4,39 @@ local class = require 'middleclass'
 local Matrix4 = class('core/Matrix4')
 
 function Matrix4:initialize( handle )
-    if handle then
-        self.handle = handle
-    else
-        self.handle = NATIVE.CreateMatrix4()
-    end
+    self.handle = handle or NATIVE.CreateMatrix4()
 end
 
 function Matrix4:copy()
-    return Matrix4.new(NATIVE.CopyMatrix4(self.handle))
+    return Matrix4:new(NATIVE.CopyMatrix4(self.handle))
 end
 
 function Matrix4:__add( other )
-    return Matrix4.new(NATIVE.AddMatrix4(self, other))
+    return Matrix4:new(NATIVE.AddMatrix4(self, other))
 end
 
 function Matrix4:__sub( other )
-    return Matrix4.new(NATIVE.SubMatrix4(self, other))
+    return Matrix4:new(NATIVE.SubMatrix4(self, other))
 end
 
 function Matrix4:__mul( other )
-    return Matrix4.new(NATIVE.MulMatrix4(self, other))
+    return Matrix4:new(NATIVE.MulMatrix4(self, other))
 end
 
 function Matrix4:__div( other )
-    return Matrix4.new(NATIVE.DivMatrix4(self, other))
+    return Matrix4:new(NATIVE.DivMatrix4(self, other))
 end
 
 function Matrix4:translate( x, y, z )
-    return Matrix4.new(NATIVE.TranslateMatrix4(self.handle, x, y, z))
+    return Matrix4:new(NATIVE.TranslateMatrix4(self.handle, x, y, z))
 end
 
 function Matrix4:scale( x, y, z )
-    return Matrix4.new(NATIVE.ScaleMatrix4(self.handle, x, y, z))
+    return Matrix4:new(NATIVE.ScaleMatrix4(self.handle, x, y, z))
 end
 
 function Matrix4:rotate( angle, x, y, z )
-    return Matrix4.new(NATIVE.RotateMatrix4(self.handle, angle, x, y, z))
+    return Matrix4:new(NATIVE.RotateMatrix4(self.handle, angle, x, y, z))
 end
 
 function Matrix4:transform( x, y, z, w )
@@ -49,7 +45,7 @@ function Matrix4:transform( x, y, z, w )
 end
 
 function Matrix4:toRotationMatrix()
-    return Matrix4.new(NATIVE.MakeRotationMatrix(self.handle))
+    return Matrix4:new(NATIVE.MakeRotationMatrix(self.handle))
 end
 
 
