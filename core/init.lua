85ddf8a8c4b048138f38a84040f54d07c42e0459
Henry Kielmann <henrykielmann@gmail.com>
2014-08-24 12:58:11 +0200

    Removed test code from core/init

--- core/init.lua
+++ core/init.lua
@@ -1,15 +1,5 @@
-package.path = '?;'..package.path
+-- Setup error handling
 
-local fileSearcher = function( name )
-    local path, message = package.searchpath(name, package.path, '/')
-    if path then
-        local loader = function( path ) return loadfile(path)() end
-        return loader, path
-    else
-        return message
-    end
-end
-
-table.insert(package.searchers, 1, fileSearcher)
-
-print('CORE/INIT LOADED')
+NATIVE.SetErrorFunction(function( message )
+    return debug.traceback(message)
+end)
