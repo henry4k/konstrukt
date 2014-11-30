3df949dcbec8527ac550e5ead357cbf735c9ed27
Henry Kielmann <henrykielmann@gmail.com>
2014-11-30 23:34:21 +0100

    Created structure dictionary.

--- core/world/Structure.lua
+++ core/world/Structure.lua
@@ -1,11 +1,17 @@
-local class = require 'middleclass'
+local class       = require 'middleclass'
 local WorldObject = require 'core/world/WorldObject'
 
 
 --- Static world objects, which are made from voxels use this as their base class.
 local Structure = class('core/world/Structure', WorldObject)
 
-function Structure:initialize()
+--- Helper function! So you don't need to import the StructureDictionary every time.
+function Structure.static:register()
+    local dict = require 'core/world/StructureDictionary'
+    dict:registerClass(self)
+end
+
+function Structure:initialize( voxelVolume, origin )
     WorldObject.initialize(self)
 end
 
