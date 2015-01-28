4f0a74bd2de4962fd79bbb5f7ce3176d36c6a8f4
Henry Kielmann <henrykielmann@gmail.com>
2015-01-28 13:06:29 +0100

    Fixed common test utils.

--- core-test/common.lua
+++ core-test/common.lua
@@ -1,6 +1,9 @@
 package.path = package.path..';third-party/lua-bdd4tap/?.lua;third-party/lua-mock/?.lua;core/third-party/?.lua'
 
+require 'core/bootstrap/math'
+require 'core/bootstrap/table'
+
 require 'Test.More'
 bdd = require 'test.bdd'
 describe = require 'test.bdd.describe'
-FakeRequire = require 'core/test/FakeRequire'
+FakeRequire = require 'core-test/FakeRequire'
