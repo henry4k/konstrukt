110aae9e35b1f2e2099dcc70e16f9817d83e7c50
Henry Kielmann <henrykielmann@gmail.com>
2015-11-21 08:55:28 +0100

    Finished BitConditionSolver

--- engine/ChunkGeneratorShared.cpp
+++ engine/ChunkGeneratorShared.cpp
@@ -1,12 +1,28 @@
+#include <assert.h>
+#include <math.h> // floor, ceil
 #include <stdlib.h> // malloc, free, realloc
 #include <string.h> // memset, memcmp, memcpy
 
 #include "ChunkGeneratorShared.h"
 
 
+static const int MAX_BIT_CONDITION_BYTES = 2;
+static const int BITS_PER_BYTE = 8;
+
+
+typedef unsigned char Byte;
+
+struct OptimizedBitCondition
+{
+    Byte values[MAX_BIT_CONDITION_BYTES];
+    Byte  masks[MAX_BIT_CONDITION_BYTES];
+    int byteCount;
+    int byteOffset;
+};
+
 struct BitConditionNode
 {
-    BitCondition condition;
+    OptimizedBitCondition condition;
 
     BitConditionNode* children;
     int childCount;
@@ -21,6 +37,8 @@ struct BitConditionSolver
 };
 
 
+static void FreeBitConditionNodeContents( BitConditionNode* node );
+
 BitConditionSolver* CreateBitConditionSolver()
 {
     BitConditionSolver* solver = new BitConditionSolver;
@@ -30,11 +48,76 @@ BitConditionSolver* CreateBitConditionSolver()
 
 void FreeBitConditionSolver( BitConditionSolver* solver )
 {
+    FreeBitConditionNodeContents(&solver->root);
     delete solver;
 }
 
-static bool AreBitConditionsEqual( const BitCondition* a,
-                                   const BitCondition* b )
+static int ceili( int a, int b )
+{
+    return ceilf((float)a / (float)b);
+}
+
+static int floori( int a, int b )
+{
+    return floorf((float)a / (float)b);
+}
+
+static Byte CreateMask( int start, int length )
+{
+    assert(length >= 0 && length <= BITS_PER_BYTE);
+    assert(start + length <= BITS_PER_BYTE);
+    const Byte bits = (1<<length)-1;
+    return bits << start;
+}
+
+// TODO: Refactor this mess :(
+static OptimizedBitCondition OptimizeBitCondition( const BitCondition in )
+{
+    assert(in.length <= MAX_BIT_CONDITION_BYTES*BITS_PER_BYTE);
+    OptimizedBitCondition out;
+    memset(&out, 0, sizeof(out));
+    out.byteCount = ceili(in.offset+in.length, BITS_PER_BYTE) -
+                    floori(in.offset, BITS_PER_BYTE);
+    out.byteOffset = floorf((float)in.offset / (float)BITS_PER_BYTE);
+    assert(out.byteCount >= 0); // == 0 could be used for funny hacks.
+    assert(out.byteCount <= MAX_BIT_CONDITION_BYTES);
+    for(int i = 0; i < out.byteCount; i++)
+    {
+        Byte value;
+        Byte mask;
+        if(out.byteCount == 1)
+        {
+            const int start = in.offset % BITS_PER_BYTE;
+            const int end = (in.offset + in.length) % BITS_PER_BYTE;
+            mask = CreateMask(start, BITS_PER_BYTE-start) &
+                   CreateMask(0, end);
+            value = in.value << start;
+        }
+        else if(i == 0) // first
+        {
+            const int start = in.offset % BITS_PER_BYTE;
+            mask = CreateMask(start, BITS_PER_BYTE-start);
+            value = in.value << start;
+        }
+        else if(i == out.byteCount-1) // last
+        {
+            const int end = (in.offset + in.length) % BITS_PER_BYTE;
+            mask = CreateMask(0, end);
+            value = in.value >> (in.length - end);
+        }
+        else // something in the middle
+        {
+            mask = CreateMask(0, BITS_PER_BYTE);
+            value = in.value >> (i*BITS_PER_BYTE);
+        }
+        out.masks[i]  = mask;
+        out.values[i] = value;
+    }
+    return out;
+}
+
+static bool AreBitConditionsEqual( const OptimizedBitCondition* a,
+                                   const OptimizedBitCondition* b )
 {
     return memcmp(a, b, sizeof(BitCondition)) == 0;
 }
@@ -54,11 +137,13 @@ static void DepositPayload( BitConditionNode* currentNode,
     }
     else
     {
+        const OptimizedBitCondition optimizedCondition =
+            OptimizeBitCondition(conditions[0]);
         BitConditionNode* childNode = NULL;
         for(int i = 0; i < currentNode->childCount; i++)
         {
             if(AreBitConditionsEqual(&currentNode->children[i].condition,
-                                     &conditions[0]))
+                                     &optimizedCondition))
             {
                 childNode = &currentNode->children[i];
                 break;
@@ -74,7 +159,7 @@ static void DepositPayload( BitConditionNode* currentNode,
 
             childNode = &currentNode->children[count-1];
             memset(childNode, 0, sizeof(BitConditionNode));
-            childNode->condition = conditions[0];
+            childNode->condition = optimizedCondition;
         }
 
         const BitCondition* nextCondition;
@@ -90,6 +175,16 @@ static void DepositPayload( BitConditionNode* currentNode,
     }
 }
 
+static void FreeBitConditionNodeContents( BitConditionNode* node )
+{
+    for(int i = 0; i < node->childCount; i++)
+        FreeBitConditionNodeContents(&node->children[i]);
+    if(node->children)
+        delete[] node->children;
+    if(node->payloadList)
+        delete[] node->payloadList;
+}
+
 void AddBitConditions( BitConditionSolver* solver,
                        const BitCondition* conditions,
                        int conditionCount,
@@ -101,10 +196,19 @@ void AddBitConditions( BitConditionSolver* solver,
                    payload);
 }
 
-static bool IsConditionMatchingBitField( const BitCondition* condition,
+static bool IsConditionMatchingBitField( const OptimizedBitCondition* condition,
                                          const void* bitField,
-                                         int bitFieldSize )
+                                         int bitFieldByteCount )
 {
+    if(condition->byteOffset + condition->byteCount > bitFieldByteCount)
+        return false;
+    for(int i = 0; i < condition->byteCount; i++)
+    {
+        const Byte bitFieldByte =
+            ((const Byte*)bitField)[condition->byteOffset + i];
+        if((bitFieldByte & condition->masks[i]) != condition->values[i])
+            return false;
+    }
     return true;
 }
 
