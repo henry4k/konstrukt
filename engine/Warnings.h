29df54e541842dd41b36e94fef42f096f6175dc8
Henry Kielmann <henrykielmann@gmail.com>
2015-12-26 18:09:52 +0100

    Fix

--- engine/Warnings.h
+++ engine/Warnings.h
@@ -5,8 +5,8 @@
 #define END_EXTERNAL_CODE   __pragma(warning(pop))
 #elif defined(__GNUC__) || defined(__clang__)
 #define BEGIN_EXTERNAL_CODE _Pragma("GCC diagnostic push") \
-                            _Pragma("GCC diagnostic ignored \"-Wpedantic\"")
-                            _Pragma("GCC diagnostic ignored \"-Wunused-variable\"")
+                            _Pragma("GCC diagnostic ignored \"-Wpedantic\"") \
+                            _Pragma("GCC diagnostic ignored \"-Wunused-variable\"") \
                             _Pragma("GCC diagnostic ignored \"-Wlong-long\"")
 #define END_EXTERNAL_CODE   _Pragma("GCC diagnostic pop")
 #else
