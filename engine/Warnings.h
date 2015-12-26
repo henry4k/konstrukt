eb45e3092a06a94f3b30101ab8598519703f54cb
Henry Kielmann <henrykielmann@gmail.com>
2015-12-26 18:05:48 +0100

    Enhanced warning macro

--- engine/Warnings.h
+++ engine/Warnings.h
@@ -7,6 +7,7 @@
 #define BEGIN_EXTERNAL_CODE _Pragma("GCC diagnostic push") \
                             _Pragma("GCC diagnostic ignored \"-Wpedantic\"")
                             _Pragma("GCC diagnostic ignored \"-Wunused-variable\"")
+                            _Pragma("GCC diagnostic ignored \"-Wlong-long\"")
 #define END_EXTERNAL_CODE   _Pragma("GCC diagnostic pop")
 #else
 #define BEGIN_EXTERNAL_CODE
